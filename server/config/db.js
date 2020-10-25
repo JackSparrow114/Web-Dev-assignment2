7ef4d4ac2de3c42d52ad38e41a4b4eba4e45c3de
kritivaish <vkriti107@gmail.com>
2020-10-25 14:58:40 -0500

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://kriti:3qysplXkXhyi1CZS@mongodbserver.0sals.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
